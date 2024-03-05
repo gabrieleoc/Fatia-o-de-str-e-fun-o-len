dc7cffee54ff5b4665787e789a28f40cab56b427
GABRIELE OLIVEIRA COSTA <116209292+gabrieleoc@users.noreply.github.com>
2024-03-05 00:05:13 -0300

    Add files via upload

---
+++ nome.py
@@ -0,0 +1,19 @@
+print('Bem vindo!')
+nome = input("Qual o seu nome?\n")
+idade = input("Qual sua idade?\n")
+
+if nome and idade:
+    print(f"Seu nome é {nome}.")
+    print(f"Seu nome invertido é {nome[::-1]}.")
+    if " " in nome:
+        print("Seu nome contém espaços.")
+    else:
+        print("Seu nome não tem espaço.")
+    
+    print(f"Seu nome tem {len(nome)} letras.")
+    print(f"A primeira letra do seu nome é {nome[0]}")
+    print(f"A última letra do seu nome é {nome[-1]}")
+   
+else:
+     print("Desculpe, você deixou campos vazios.")
+    
